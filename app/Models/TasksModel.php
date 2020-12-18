eb3a311b227e2e2aceffb7bd14b73a634f27d7b6
kei.amishima <kei.prs59@gamil.com>
2020-12-18 19:45:15 +0900

    model側実装

--- app/Models/TasksModel.php
+++ app/Models/TasksModel.php
@@ -7,11 +7,18 @@ class TasksModel extends Model
     protected $table = 'tb_tasks';
     protected $allowedFields = ['title', 'description', 'end_date', 'completed', 'weight'];
 
+    /**
+     * 全件取得
+     */
     public function getTaskAll()
     {
         return $this->findAll();
     }
 
+    /**
+     * 1件取得
+     * @param int $id タスクID
+     */
     public function getTaskById($id)
     {
         return $this->asArray()
@@ -19,8 +26,25 @@ class TasksModel extends Model
             ->first();
     }
 
-    public function setTask($data)
+    /**
+     * saveメソッドはレコードが存在しなければinsert, 存在すればupdateを実行.
+     * @param array $data $this->allowedFields
+     * @note  $dataにidを含めた場合はupdate
+     */
+    public function saveTask($data)
     {
         return $this->save($data);
     }
+
+    // FIXME 完了/ 未完了フラグ更新向けの処理を作る. できればトグルで.
+
+    /**
+     * 1件削除
+     * @param int $id タスクID
+     */
+    public function deleteTask($id)
+    {
+        return $this->delete($id);
+    }
+
 }
